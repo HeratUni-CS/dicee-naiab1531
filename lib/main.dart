723f34c93a9c95740a3efaaa8a9b0820e0b92e77
Rahimi1531 <naiab1531@gmail.com>
2023-06-07 13:18:39 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,69 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice'),
-        ),
-        body: MyApp(),
-      ),
-    ),
-  );
+  return (runApp(MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+int num = 1;
+int num1 = 1;
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Dice Game'),
+          ),
+          backgroundColor: Color.fromARGB(255, 199, 176, 171),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(),
+                    child: Image.asset('images/dice-1.png'),
+                    onPressed: () {
+                      setState(() {
+                        num = Random().nextInt(6) + 1;
+                        num1 = Random().nextInt(6) + 1;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    style: TextButton.styleFrom(),
+                    child: Image.asset('images/dice-${num1}.png'),
+                    onPressed: () {
+                      setState(() {
+                        num1 = Random().nextInt(6) + 1;
+                        num = Random().nextInt(6) + 1;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
